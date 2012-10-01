9eb77fe3cca4bd2350b01173e7ebaacdb059ed12
Peter Russell <qwertyface@gmail.com>
2012-10-01 14:40:35 +0100

    Make the tests pass

--- ote/__main__.py
+++ ote/__main__.py
@@ -2,8 +2,8 @@
 
 Usage:
   ote help [<subcommand>]
-  ote lint [path]
-  ote test [path]
+  ote lint [<path>]
+  ote test [<path>]
 
 """
 import sys;
@@ -12,12 +12,31 @@ import docopt
 
 from ote import __version__
 
-def main(argv=sys.argv, stdin=sys.stdin, stdout=sys.stdin,
+
+def help_(stdin, stdout, stderr, **kwargs):
+    print >> stdout, docopt.printable_usage(__doc__)
+
+
+def lint(stdin, stdout, stderr, **kwargs):
+    print >> stdout, "Linting", kwargs.get('<path>', '')
+
+
+SUBCOMMANDS = {
+    'help': help_,
+    'lint': lint
+}
+
+
+def main(argv=sys.argv, stdin=sys.stdin, stdout=sys.stdout,
          stderr=sys.stderr):
     """The main entry point"""
     arguments = docopt.docopt(__doc__, argv[1:], version=__version__)
-    print arguments
-    
+    selected_subcommand, = (key for key, value in arguments.iteritems()
+                            if value is True and not key.startswith('<'))
+    subcommand = SUBCOMMANDS[selected_subcommand]
+    subcommand(stdin, stdout, stderr, **arguments)
+    return 0
+
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OTE-IDE: The One True Editor IDE",
-    version="0.1",
+    version="0.1", # TODO: Get from __version__.py
     packages=find_packages(),
     entry_points = {'console_scripts': ['ote = ote.__main__:main']},
     install_requires=['mock', 'nose', 'docopt'],
